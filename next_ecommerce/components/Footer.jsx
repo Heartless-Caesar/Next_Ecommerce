d738dd0cf492a27eebab4fc18d020bafad1db516
caesar <joaovictorcsatu@gmail.com>
2022-07-29 00:48:32 -0300

    update

--- next_ecommerce/components/Footer.jsx
+++ next_ecommerce/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>F</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 - Alpha Cleaning Services All rights reserved</p>
+    </div>
+  );
 };
 
 export default Footer;
