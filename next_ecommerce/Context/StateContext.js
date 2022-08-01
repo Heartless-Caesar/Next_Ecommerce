2f218dc885837ca5453eaabba7faab67d2bb78d3
caesar <joaovictorcsatu@gmail.com>
2022-07-31 23:41:30 -0300

    Update to add to cart logic

--- next_ecommerce/Context/StateContext.js
+++ next_ecommerce/Context/StateContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 
+//BASE CONTEXT
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
@@ -10,10 +11,36 @@ export const StateContext = ({ children }) => {
   const [totalQuantity, setTotalQuantity] = useState(0);
   const [quantity, setQuantity] = useState(1);
 
+  //ADD PRODUCT TO CART
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantity((prevTotalQuantity) => prevTotalQuantity + quantity);
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id) {
+          return { ...cartProduct, quantity: cartProduct.quantity + quantity };
+        }
+      });
+      setCartItems(updatedCartItems);
+      toast.success(`${quantity} ${product.name} added to the cart`);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+  };
+
+  //INCREASE ITEM QUANTITY FOR PURCHASE
   const increaseQty = () => {
     setQuantity((prevQty) => prevQty + 1);
   };
 
+  //DECREASE ITEM QUANTITY FOR PURCHASE
   const decreaseQty = () => {
     setQuantity((prevQty) => {
       if (prevQty - 1 < 1) {
@@ -40,4 +67,6 @@ export const StateContext = ({ children }) => {
   );
 };
 
+//FUNCTION USED TO CALL THE CONTEXT
+//DESTRUCTURE THE CONTEXT ELEMENTS YOU DESIRE TO USE FROM IT
 export const useStateContext = () => useContext(Context);
